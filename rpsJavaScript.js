4e96330adef50a880ad82b58389b57ca8343184c
Brandon <brandon.r.young@outlook.com>
2022-07-07 15:13:38 -0400

    Finish RPS UI

--- rpsJavaScript.js
+++ rpsJavaScript.js
@@ -1,104 +1,100 @@
-let rock = document.querySelector('.rock')
-        rock.addEventListener('click', work = () => {
-            playRound('Rock', computerPlay());
-        }); // plays with player selection as rock when rock button is pressed
+let rock = document.querySelector('.choiceRock')
+let paper = document.querySelector('.choicePaper')
+let scissors = document.querySelector('.choiceScissors')
 
-        let paper = document.querySelector('.paper')
-        paper.addEventListener('click', work1 = () => {
-            playRound('Paper', computerPlay());
-        });
+//makes rock paper and scissor buttons work
+rock.addEventListener('click', work = () => {
+    playRound('Rock', computerPlay());
+}); // plays with player selection as rock when rock button is pressed
+paper.addEventListener('click', work1 = () => {
+    playRound('Paper', computerPlay());
+})
+scissors.addEventListener('click', work2 = () => {
+    playRound('Scissors', computerPlay());
+});
 
-        let scissors = document.querySelector('.scissors')
-        scissors.addEventListener('click', work2 = () => {
-            playRound('Scissors', computerPlay());
-        });
+let computerChoice = ["Rock","Paper","Scissors"]
 
-        let computerChoice = ["Rock","Paper","Scissors"];
+// determines computers play
+let computerPlay = () => computerChoice[Math.floor(Math.random() * computerChoice.length)]
 
-        // determines computers play
-        let computerPlay = () => computerChoice[Math.floor(Math.random() * computerChoice.length)];
-
-        //plays a round of Rock Papaer Scissors and adds to scoreboard
-        let playRound = (playerSelection, computerSelection) => {
-
-            if (playerSelection === computerSelection) {
-                gameText.textContent = `It's A draw! You both chose ${playerSelection}`   
-            } else if (playerSelection === "Rock") {
-                if (computerSelection === "Paper") {
-                    computerScore.textContent = parseInt(computerScore.textContent)+1;
-                    gameText.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`   
-                    winningScore();
-                } else {
-                    playerScore.textContent = parseInt(playerScore.textContent)+1;
-                    gameText.textContent = `You Win! ${playerSelection} beats ${computerSelection}`   
-                    winningScore();  
-                }
-            } else if (playerSelection === "Paper") {
-                if (computerSelection === "Rock") {
-                    playerScore.textContent = parseInt(playerScore.textContent)+1; 
-                    gameText.textContent = `You Win! ${playerSelection} beats ${computerSelection}`   
-                    winningScore();               
-                } else {
-                    computerScore.textContent = parseInt(computerScore.textContent)+1;
-                    gameText.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`   
-                    winningScore();
-                }
-            } else {
-                if (computerSelection === "Rock") {
-                    computerScore.textContent = parseInt(computerScore.textContent)+1;
-                    gameText.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`   
-                    winningScore();
-                } else {
-                    playerScore.textContent = parseInt(playerScore.textContent)+1;
-                    gameText.textContent = `You Win! ${playerSelection} beats ${computerSelection}`   
-                    winningScore();            
-                }
-            }
+//plays a round of Rock Papaer Scissors and adds to scoreboard
+let playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        gameText.textContent = `It's A draw! You both chose ${playerSelection}.`   
+    } else if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            computerScore.textContent = parseInt(computerScore.textContent)+1;
+            gameText.textContent = `You Lose! ${computerSelection} beats ${playerSelection}.`   
+            winningScore();
+        } else {
+            playerScore.textContent = parseInt(playerScore.textContent)+1;
+            gameText.textContent = `You Win! ${playerSelection} beats ${computerSelection}.`   
+            winningScore();  
+        }
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            playerScore.textContent = parseInt(playerScore.textContent)+1; 
+            gameText.textContent = `You Win! ${playerSelection} beats ${computerSelection}.`   
+            winningScore();               
+        } else {
+            computerScore.textContent = parseInt(computerScore.textContent)+1;
+            gameText.textContent = `You Lose! ${computerSelection} beats ${playerSelection}.`   
+            winningScore();
+        }
+    } else {
+        if (computerSelection === "Rock") {
+            computerScore.textContent = parseInt(computerScore.textContent)+1;
+            gameText.textContent = `You Lose! ${computerSelection} beats ${playerSelection}.`   
+            winningScore();
+        } else {
+            playerScore.textContent = parseInt(playerScore.textContent)+1;
+            gameText.textContent = `You Win! ${playerSelection} beats ${computerSelection}.`   
+            winningScore();            
+        }
+    }
+}
         
-                
-        }
 
-        // Determines if someone has reached the winning score. Disables Rock, Paper, and Scissors buttons.
-        const winningScore = () => {
-            if (parseInt(playerScore.textContent) === 5) {
-                gameText.textContent = 'You Win! You conquered the computer!' 
-                btnDisable();       
-            } 
-            if (parseInt(computerScore.textContent) === 5) {
-                gameText.textContent = 'You Lose! The computer conquered you!'
-                btnDisable();
-            }
-        };
+// Determines if someone has reached the winning score. Disables Rock, Paper, and Scissors buttons.
+const winningScore = () => {
+    if (parseInt(playerScore.textContent) === 5) {
+        gameText.textContent = 'You Win! You conquered the computer!' 
+        btnDisable();       
+    } 
+    if (parseInt(computerScore.textContent) === 5) {
+        gameText.textContent = 'You Lose! The computer conquered you!'
+        btnDisable();
+    }
+}
+// Adds new game button that resets scores and reenables Rock, Paper, and Scissors buttons
+const newGame = () => {
+    const reset = document.createElement('button');
+    reset.className = 'reset'
+    reset.textContent = 'New Game'
+    resetGame.appendChild(reset);
+    reset.addEventListener('click', newG = () => {
+        computerScore.textContent = 0;
+        playerScore.textContent = 0;
+        gameText.textContent = "";
+        btnEnable();
+    });    
+}
 
-        // Adds new game button that resets scores and reenables Rock, Paper, and Scissors buttons
-        const newGame = () => {
-            const reset = document.createElement('button');
-            reset.textContent = 'New Game'
-            gameInfo.appendChild(reset);
-            reset.addEventListener('click', newG = () => {
-                computerScore.textContent = 0;
-                playerScore.textContent = 0;
-                gameText.textContent = "";
-                btnEnable();
-            });    
-        }
-        
-        // Disables and reenables Rock Paper and Scissor buttons
-        const btnDisable = () => {
-            document.querySelector('.rock').disabled = true;
-            document.querySelector('.paper').disabled = true;
-            document.querySelector('.scissors').disabled = true;
-        }
-
-        const btnEnable = () => {
-            document.querySelector('.rock').disabled = false;
-            document.querySelector('.paper').disabled = false;
-            document.querySelector('.scissors').disabled = false;
-        }
-
-        let gameInfo = document.querySelector('.gameInfo')
-        let gameText = document.querySelector('.gameText');
-        let playerScore = document.getElementById('playerScore');
-        let computerScore = document.getElementById('computerScore');
-
-        newGame();
+// Disables and reenables Rock Paper and Scissor buttons
+const btnDisable = () => {
+    document.querySelector('.choiceRock').disabled = true;
+    document.querySelector('.choicePaper').disabled = true;
+    document.querySelector('.choiceScissors').disabled = true;
+    
+}
+const btnEnable = () => {
+    document.querySelector('.choiceRock').disabled = false;
+    document.querySelector('.choicePaper').disabled = false;
+    document.querySelector('.choiceScissors').disabled = false;
+}
+let resetGame = document.querySelector('.resetGame')
+let gameText = document.querySelector('.gameText');
+let playerScore = document.getElementById('playerScore');
+let computerScore = document.getElementById('computerScore')
+newGame();
